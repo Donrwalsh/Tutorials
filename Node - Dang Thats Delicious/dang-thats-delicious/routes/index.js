33ec67e24c130e9775417439c956dd3a1846746b
Bigbrass <donrwalsh@gmail.com>
2017-06-02 11:35:35 -0500

    Completed: Node 04 - Core Concept - Routing
    Discussion of Express Framework and how it handles routes, including
    some cool things you can do to make simple endpoints using routes. Find
    examples in index.js

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -3,7 +3,15 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  const wes = { name: 'Wes', age: 100, cool: true };
+  //res.json(wes) //Only send once, more times will give an error
+  res.send(req.query.name); //For URL data submission localhost:7777/?name=Don
+
+});
+
+router.get('/reverse/:name', (req, res) => {
+	const reverse = [...req.params.name].reverse().join(''); // reverses the string req.params.name
+	res.send(reverse);
 });
 
 module.exports = router;
