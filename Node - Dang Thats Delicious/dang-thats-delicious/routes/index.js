fc143c5a94f4e2812acc093ed5be27a9e19f0b1f
Bigbrass <donrwalsh@gmail.com>
2017-06-02 17:58:44 -0500

    Completed: Node 07 - Core Concept - Controllers and the MVC Pattern
    Here we remove the code from the actual route and instead call a method
    that we create to handle the specifics freeing our route file to be
    simply routes. MVC Pattern

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -1,18 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  const wes = { name: 'Wes', age: 100, cool: true };
-  //res.json(wes) //Only send once, more times will give an error
-  //res.send(req.query.name); //For URL data submission localhost:7777/?name=Don
-  res.render('hello', { 
-  	name: 'wes',
-  	dog: req.query.dog,
-  	title: 'I love food'
-  });
-
-});
+router.get('/', storeController.homePage)
 
 router.get('/reverse/:name', (req, res) => {
 	const reverse = [...req.params.name].reverse().join(''); // reverses the string req.params.name
