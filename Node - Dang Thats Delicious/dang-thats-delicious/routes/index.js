36d595aa8dbb5e7d6a0ec45d180c7649a56435de
Bigbrass <donrwalsh@gmail.com>
2017-06-03 10:04:18 -0500

    Completed: Node 10 - Saving Stores and using Mixins
    Here we built the form for the Add part of the website, allowing you to
    add new stores. Done by use of Mixin with current routing simply
    reporting back with JSON of what was submitted in browser and console.

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -4,5 +4,7 @@ const storeController = require('../controllers/storeController');
 
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
