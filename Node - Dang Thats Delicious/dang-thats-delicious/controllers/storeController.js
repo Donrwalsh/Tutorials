d39b920397eb669761b76cb4891271af4cb1f8ea
Bigbrass <donrwalsh@gmail.com>
2017-06-03 09:17:25 -0500

    Completed: Node 08 - Core Concept - Middleware and Error Handling
    Walked through the Plinko Machine of app.js and error handling with an
    example of an error. Run it to see the errorHandler.js do its magic
    with stack trace and all (Dev env is specified in variables.env)

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,12 @@
+exports.myMiddleware = (req, res, next) => {
+	req.name = 'Don';
+	if(req.name === 'Don') {
+		throw Error('That is a stupid name');
+	}
+	next();
+};
+
 exports.homePage = (req, res) => {
+	console.log(req.name);
 	res.render('index');
 };
--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -2,12 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', storeController.homePage)
 
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join(''); // reverses the string req.params.name
-	res.send(reverse);
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
 module.exports = router;
