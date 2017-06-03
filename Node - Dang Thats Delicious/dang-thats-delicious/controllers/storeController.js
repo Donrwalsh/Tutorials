e735974033d805862d756d64bbb83b3c3fbc3261
Bigbrass <donrwalsh@gmail.com>
2017-06-03 09:33:32 -0500

    Completed: Node 09 - Core Concept - Creating our Store Model
    Example of how to create a database schema for use in MongoDB. Store.js
    shows this simple starting schema while the changes to start.js import
    it. Other changes are cleanup from previous examples.

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -1,12 +1,3 @@
-exports.myMiddleware = (req, res, next) => {
-	req.name = 'Don';
-	if(req.name === 'Don') {
-		throw Error('That is a stupid name');
-	}
-	next();
-};
-
 exports.homePage = (req, res) => {
-	console.log(req.name);
 	res.render('index');
 };
--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
 
 module.exports = router;
