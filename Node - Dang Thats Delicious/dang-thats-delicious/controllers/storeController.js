412677f3d048c5bcc1e379614f61c31224066f1d
Bigbrass <donrwalsh@gmail.com>
2017-06-04 11:21:02 -0500

    Completed: Node 12 - Flash Messages
    Using Flash Messages to alert user to successful event (adding a new
    store here). Also wraps the saving of the store and awaits it so we can
    redirect to a new link using the slug that is created on save.

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
+	console.log(req.name);
 	res.render('index');
 };
 
@@ -10,8 +11,9 @@ exports.addStore = (req, res) => {
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully Created ${store.name}. 
+		Care to leave a review?`);
 	console.log('It worked!');
-	res.redirect('/');
+	res.redirect(`/store/${store.slug}`);
 };
--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
-//Object destructuring, allows us to import a single method rather than the whole file:
 const { catchErrors } = require('../handlers/errorHandlers'); 
-
 
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
