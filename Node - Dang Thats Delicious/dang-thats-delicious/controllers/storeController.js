ed3ba1e81210c00d9e02ec2086865b17605f05d4
Bigbrass <donrwalsh@gmail.com>
2017-06-04 11:39:44 -0500

    Completed: Node 13 - Querying our Database for Stores
    Simple example of grabbing stores from the database to populate the
    /stores page (and the homepage). Done by wrapping both routes in
    catchErrors() (for async/await), getStores from storeController renders
    the new stores pug file using storeCards.

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -17,3 +17,10 @@ exports.createStore = async (req, res) => {
 	console.log('It worked!');
 	res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+	// 1. Query the database for a list of all stores
+	const stores = await Store.find();
+	console.log(stores)
+	res.render('stores', { title: 'Stores', stores})
+}
