2b0129cf1d2a215ce257599da15e9cad7e2e75bd
Bigbrass <donrwalsh@gmail.com>
2017-06-04 09:56:24 -0500

    Completed: Node 11 - Using Async Await
    Now we actually save data to the database via Async Await functions. In
    storeController.js, L15 will not happen until L14 promise is completed.
    Also using the catchErrors middleware on the route itself.

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -1,13 +1,17 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
 	res.render('index');
 };
-
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store'});
 };
 
-exports.createStore = (req, res) => {
-	console.log(req.body)
-	res.json(req.body)
-}
+exports.createStore = async (req, res) => {
+	const store = new Store(req.body);
+	await store.save();
+	console.log('It worked!');
+	res.redirect('/');
+};
--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+//Object destructuring, allows us to import a single method rather than the whole file:
+const { catchErrors } = require('../handlers/errorHandlers'); 
 
 
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
