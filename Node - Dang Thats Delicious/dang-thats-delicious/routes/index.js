06f1da352b1a72e2695bca97e4719ea2507fb067
Bigbrass <donrwalsh@gmail.com>
2017-06-07 11:37:28 -0500

    Completed: Node 24 - Virtual Fields, Login:Logout middleware and Protecting Rotues
    Configuring the login/logout functionality to go along with register.
    Also demonstrating how to use it with middleware by restricting the
    ‘/add’ page to logged in users.

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -7,7 +7,8 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn,
+				   storeController.addStore);
 
 router.post('/add', 
 	storeController.upload, 
@@ -28,6 +29,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // 1. Validate the registration data
@@ -36,5 +38,9 @@ router.get('/register', userController.registerForm);
 router.post('/register', userController.validateRegister,
 						 userController.register,
 						 authController.login);
+
+router.get('/logout', authController.logout);
+
+
 
 module.exports = router;
