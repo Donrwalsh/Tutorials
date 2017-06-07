b3ab4dbe8904bc75a18db830f989d8231e959fe1
Bigbrass <donrwalsh@gmail.com>
2017-06-06 21:45:16 -0500

    Completed: Node 22 - Creating User Accounts
    Shell functionality for users submitting a registration form to create
    an account in the new user Schema. Struggled a bit because I tried to
    declare this as a mixing like before, but something about doing that
    busts this functionality. As of now the form simply accepts data and
    reports on errors, nothing is done with received data.

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers'); 
 
 router.get('/', catchErrors(storeController.getStores));
@@ -24,5 +25,13 @@ router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// 1. Validate the registration data
+// 2. Register the user
+// 3. We need to log them in
+router.post('/register', userController.validateRegister);
 
 module.exports = router;
