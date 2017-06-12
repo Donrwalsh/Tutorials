98251e97d2a86d03c6c007621495979e13c38515
Bigbrass <donrwalsh@gmail.com>
2017-06-11 20:33:46 -0500

    Completed: Node 37 - Adding a Reviews Data Model
    Data Model for Reviews + a form for submitting them that gets attached
    to the bottom of individual store pages. Some funky business happening
    with the stars on this form, mostly handled via css.

--- Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 const { catchErrors } = require('../handlers/errorHandlers'); 
 
 router.get('/', catchErrors(storeController.getStores));
@@ -51,6 +52,7 @@ router.post('/account/reset/:token', authController.confirmedPasswords,
 
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts))
+router.post('/reviews/:id', authController.isLoggedIn, catchErrors(reviewController.addReview))
 
 /*
 	API
