86c6b47a4bf6424b4654fae7443f8cbdbbaf905c
Bigbrass <donrwalsh@gmail.com>
2017-06-12 20:20:41 -0500

    Completed: Node 40 - Implementing Pagination
    Quickly building pagination for our Stores display. Uses some simple
    arithmetic to determine how many pages is max and will return you to
    the max page if you try to navigate to one that is higher.

--- Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
+++ Node - Dang Thats Delicious/dang-thats-delicious/controllers/storeController.js
@@ -54,9 +54,31 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+	const page = req.params.page || 1;
+	const limit = 4;
+	const skip = (page * limit) - limit;
+
 	// 1. Query the database for a list of all stores
-	const stores = await Store.find().populate('reviews');
-	res.render('stores', { title: 'Stores', stores})
+	
+	const storesPromise = Store
+		.find().populate('reviews')
+		.skip(skip)
+		.limit(limit)
+		.sort({ created: 'desc'});
+
+	const countPromise = Store.count();
+
+	const [stores, count] = await Promise.all([storesPromise, countPromise]);
+
+	const pages = Math.ceil(count / limit);
+	if (!stores.length && skip) {
+		req.flash('info', `Hey! you asked for page ${page}. But that doesn't exist. So I put you on page ${pages}`);
+		res.redirect(`/stores/page/${pages}`);
+		return;
+	};
+
+
+	res.render('stores', { title: 'Stores', stores, page, pages, count})
 };
 
 const confirmOwner = (store, user) => {
