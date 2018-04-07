94b8a7e1847d7d53e5da400dfb21b2eba4fc12a2
Don <donrwalsh@gmail.com>
2018-04-07 14:21:04 -0500

    Java Essentials - Debugging
    Nothing much in the code, but is useful to capture for the specific breakpoint examples described in my notes.

---
+++ Java - Essential Training/Debugging/src/com/example/java/Main.java
@@ -0,0 +1,14 @@
+package com.example.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String welcome = "Welcome!";
+        char[] chars = welcome.toCharArray();
+        for (char character :
+             chars) {
+            System.out.println(character);
+        }
+    }
+}
