74d683b1d6faae22c84ad844fb0d26ccca600bbb
Don <donrwalsh@gmail.com>
2018-04-08 18:06:16 -0500

    Java Essentials - Enums
    Simple example of using enumerations instead of constants for handling operations. Retains the same functionality as last time but is being called with a custom enum 'Operation' instead of a string (or int).

---
+++ Java - Essential Training/Enums/src/com/example/java/Main.java
@@ -0,0 +1,27 @@
+package com.example.java;
+
+import com.example.java.utilities.MathHelper;
+import com.example.java.utilities.Operation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MathHelper helper = new MathHelper(100);
+
+        try {
+            while (true) {
+                int input = helper.doMath("Enter a number: ", Operation.SUBTRACT);
+                int total = helper.getTotal();
+                String message = String.format(
+                        "Entered: %d, total = %d", input, total
+                );
+                System.out.println(message);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("All done!");
+        }
+
+    }
+
+}
